f854381fb56510b8100e4c26c3d53f5953b98065
Phoenix <fy24h.huang@gmail.com>
2023-07-05 20:52:46 +0800

    update build.sh

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,5 +1,5 @@
 #bin/bash
-echo "build package"
+echo "build core start"
 cp -r "${GITHUB_WORKSPACE}/.github/workflows/package-core.json" "${GITHUB_WORKSPACE}/core/package.json"
 cp -r "${GITHUB_WORKSPACE}/.github/workflows/tsconfig-core.json" "${GITHUB_WORKSPACE}/core/tsconfig.json"
 # cd "${GITHUB_WORKSPACE}/core" || exit 1
@@ -9,28 +9,43 @@ npm install
 
 echo "tsc start"
 tsc
-echo "tsc end"
-ls -al "${GITHUB_WORKSPACE}"
-ls -al "${GITHUB_WORKSPACE}/core"
-ls -al "${GITHUB_WORKSPACE}/build"
-# for service in `ls "${GITHUB_WORKSPACE}/services`
-#     do
-#     cd "${GITHUB_WORKSPACE}/services/${service}" || exit 1
-#     # get the first version
-#     first=true
-#     for version in `ls`
-#     do
-#         # do something
-#         if test -d "${version}"
-#         then
-#         if ${first}; then
-#             echo "export * from './${version}/public-api';" >> "huaweicloud-sdk-${service}.ts"
-#             first=false
-#         fi
+echo "tsc end" 
+echo "ls -al build/core"
 
-#         echo "export * as ${version} from './${version}/public-api';" >> "huaweicloud-sdk-${service}.ts"
-#         fi
-#     done
-# done
-# cd "${GITHUB_WORKSPACE}" || exit 1
-# tsc
+ls -al "${GITHUB_WORKSPACE}/build/core"
+cat "${GITHUB_WORKSPACE}/build/core/package.json"
+
+echo "build core end"
+
+echo "build services start"
+
+cp -r "${GITHUB_WORKSPACE}/.github/workflows/package-service.json" "${GITHUB_WORKSPACE}/services/package.json"
+cp -r "${GITHUB_WORKSPACE}/.github/workflows/tsconfig-service.json" "${GITHUB_WORKSPACE}/services/tsconfig.json"
+# cd "${GITHUB_WORKSPACE}/core" || exit 1
+ls -al "${GITHUB_WORKSPACE}/services"
+cd "${GITHUB_WORKSPACE}/services"
+npm install 
+
+cp -r "${GITHUB_WORKSPACE}/build/core" "${GITHUB_WORKSPACE}/services/node_modules/@huaweicloud/huaweicloud-sdk-core"
+
+for service in `ls "${GITHUB_WORKSPACE}/services`
+    do
+    cd "${GITHUB_WORKSPACE}/services/${service}" || exit 1
+    # get the first version
+    first=true
+    for version in `ls`
+    do
+        # do something
+        if test -d "${version}"
+        then
+        if ${first}; then
+            echo "export * from './${version}/public-api';" >> "huaweicloud-sdk-${service}.ts"
+            first=false
+        fi
+
+        echo "export * as ${version} from './${version}/public-api';" >> "huaweicloud-sdk-${service}.ts"
+        fi
+    done
+done 
+tsc
+echo "build services end"
