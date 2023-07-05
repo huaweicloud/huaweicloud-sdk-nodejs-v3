5704136846e0798af0a934b1ae80cec35708fa20
Phoenix <fy24h.huang@gmail.com>
2023-07-05 08:56:26 +0800

    update main.yaml

---
+++ .github/workflows/build.sh
@@ -0,0 +1,25 @@
+#bin/bash
+echo "build package"
+cp -r "${GITHUB_WORKSPACE}/.github/workflows/package-core.json" "${GITHUB_WORKSPACE}/core/package.json"
+
+# for service in `ls "${GITHUB_WORKSPACE}/services`
+#     do
+#     cd "${GITHUB_WORKSPACE}/services/${service}" || exit 1
+#     # get the first version
+#     first=true
+#     for version in `ls`
+#     do
+#         # do something
+#         if test -d "${version}"
+#         then
+#         if ${first}; then
+#             echo "export * from './${version}/public-api';" >> "huaweicloud-sdk-${service}.ts"
+#             first=false
+#         fi
+
+#         echo "export * as ${version} from './${version}/public-api';" >> "huaweicloud-sdk-${service}.ts"
+#         fi
+#     done
+# done
+# cd "${GITHUB_WORKSPACE}" || exit 1
+# tsc
